5ad74e9e94561b62d8c5a767cdb6b322265cd714
requinard <david.diks@student.fontys.nl>
2016-03-30 15:23:24 +0200

    Added achievement admins

--- achievements/admin.py
+++ achievements/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Achievement)
-admin.site.register(AchievementGet)
+
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "date_last_accessed")
+    list_filter = ("date_last_accessed",)
+
+class AchievementGetAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "achievement", "awarded_by", "score")
+    list_filter = ("user", "achievement", "awarded_by")
+
+admin.site.register(Achievement, AchievementAdmin)
+admin.site.register(AchievementGet, AchievementGetAdmin)
