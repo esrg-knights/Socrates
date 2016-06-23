950ea83b066f1b383f0b88c3c2ae42b52324b8cf
Requinard <contact@terarion.com>
2016-06-24 00:08:09 +0200

    updated detailsmodel admin field

--- account/admin.py
+++ account/admin.py
@@ -5,8 +5,23 @@ from account.models import DetailsModel, PasswordChangeRequestModel
 
 
 class DetailsModelAdmin(admin.ModelAdmin):
-    list_display = ("id", "related_user")
-    list_filter = ("theme",)
+    list_display = ("id",
+                    "related_user",
+                    "get_first_name",
+                    "get_last_name",
+                    "nickname",
+                    "receive_broadcasts",
+                    "is_softbanned"
+                    )
+    list_filter = ("theme", "is_softbanned", "instituut", "receive_broadcasts")
+    search_fields = ("related_user",)
+
+    def get_first_name(self, obj):
+        return obj.related_user.first_name
+
+    def get_last_name(self, obj):
+        return obj.related_user.last_name
+
 
 admin.site.register(DetailsModel, DetailsModelAdmin)
 admin.site.register(PasswordChangeRequestModel)
