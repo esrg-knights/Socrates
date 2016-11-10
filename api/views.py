c776d0bfb29e230ab969fc91c83167530c1d32bd
requinard <d.diks94@gmail.com>
2016-11-10 13:52:01 +0100

    Added pagination to API's

--- api/views.py
+++ api/views.py
@@ -7,27 +7,37 @@ from api.serializers import DinnerListSerializer, UserWithProfile, AchievementSe
 from dining.models import DiningList
 
 
-class StandardResultsSetPagination(PageNumberPagination):
+class SinglePagePagination(PageNumberPagination):
     page_size = 1
     page_size_query_param = 'page_size'
     max_page_size = 1
 
 
+class StandardPaginationation(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
 class DinnerViewSet(ModelViewSet):
     queryset = DiningList.objects.all().order_by("-pk")
     serializer_class = DinnerListSerializer
-    pagination_class = StandardResultsSetPagination
+    pagination_class = SinglePagePagination
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserWithProfile
+    pagination_class = StandardPaginationation
 
 
 class AchievementViewSet(ModelViewSet):
     queryset = Achievement.objects.all()
     serializer_class = AchievementSerializer
+    pagination_class = StandardPaginationation
+
 
 class AchievementGetViewSet(ModelViewSet):
     queryset = AchievementGet.objects.all().order_by('-score', '-date_achieved')
     serializer_class = SimpleAchievementGetSerializer
+    pagination_class = StandardPaginationation
