230a1ce84ba90141fdfb2fcac8e99aab434b175d
requinard <d.diks94@gmail.com>
2017-04-28 14:40:59 +0200

    refactor: remove old views and forms
    Views and forms will no longer be user so they are removed

--- achievements/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, patterns
-
-from achievements.views import IndexView
-
-urlpatterns = patterns(
-    '',
-    url(r'^', IndexView.as_view(), name="index"),
-)
--- news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, patterns
-
-from news.views import IndexView
-
-urlpatterns = patterns(
-    '',
-    url(r'^', IndexView.as_view(), name="index")
-)
